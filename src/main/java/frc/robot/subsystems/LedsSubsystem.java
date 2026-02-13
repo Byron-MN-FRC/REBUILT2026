473ac93c21bc248224f729e1b298dc8782a1e0b6
Catburger12 <bdowne32@students.byron.k12.mn.us>
2026-02-13 17:29:17 -0600

    dont mind me

--- src/main/java/frc/robot/subsystems/LedsSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedsSubsystem.java
@@ -16,6 +16,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.ColorLED;
 import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.subsystems.ClimbSubsystem.LockdownMode;
+// import frc.robot.subsystems.Climb.LockdownMode;
 
 public class LedsSubsystem extends SubsystemBase {
     public enum LedColor {
@@ -88,6 +91,15 @@ public class LedsSubsystem extends SubsystemBase {
         if (ledColor == LedColor.redFlashing) {
             lightStrip.redFlashing();
         }
+        // if (Robot.getInstance().m_climb.currentLockdownMode == LockdownMode.full) {
+        //     lightStrip.redFlashing();
+        // }
+        // else if (Robot.getInstance().m_climb.currentLockdownMode == LockdownMode.partial) {
+        //     lightStrip.red();
+        // }
+        // else{
+            
+        // }
     }
 
     @Override
