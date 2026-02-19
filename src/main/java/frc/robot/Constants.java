4ae7f712db2b8157d9e21fa5094677960441a10f
mhelve46 <miles.helvey@gmail.com>
2026-02-19 15:41:58 -0600

    Add intake existence check to Hopper subsystem and update constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
 
     public static final class Debug {
         public static final boolean DEBUG_MODE = false;
+        public static final boolean INTAKE_EXISTS = false;
     }
 
     public static final class DriveConstants {
