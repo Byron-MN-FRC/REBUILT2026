4005d85b3e38d35a71ab58e095ca14120d96a84a
Paxton Reeve <paxtonreeve@gmail.com>
2026-02-20 19:37:00 -0600

    zeroing

--- src/main/java/frc/robot/commands/AutonStart.java
+++ src/main/java/frc/robot/commands/AutonStart.java
@@ -1,13 +1,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.Turret;
 
 public class AutonStart extends SequentialCommandGroup {
-    public AutonStart(Turret turret, Hopper hopper) {
+    public AutonStart(Turret turret, ClimbSubsystem climb) {
         addCommands(
-            new ZeroAll(turret, hopper)
+            new ZeroAll(turret, climb)
         );
     }
     
--- src/main/java/frc/robot/commands/ZeroAll.java
+++ src/main/java/frc/robot/commands/ZeroAll.java
@@ -1,14 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.Turret;
 
 public class ZeroAll extends SequentialCommandGroup {
-    public ZeroAll(Turret turret, Hopper hopper) {
+    public ZeroAll(Turret turret, ClimbSubsystem climb   ) {
         addCommands(
             new ZeroTurret(turret),
-            new ZeroHopper(hopper)
+            new ZeroClimb(climb)
         );
     }
 
