c7cd46d4d1e94dfe193dc228b1c8f32e8588745c
Paxton Reeve <paxtonreeve@gmail.com>
2026-02-20 19:46:57 -0600

    hey

--- src/main/java/frc/robot/commands/ZeroAll.java
+++ src/main/java/frc/robot/commands/ZeroAll.java
@@ -2,13 +2,14 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ClimbSubsystem;
+import frc.robot.subsystems.LedsSubsystem;
 import frc.robot.subsystems.Turret;
 
 public class ZeroAll extends SequentialCommandGroup {
-    public ZeroAll(Turret turret, ClimbSubsystem climb   ) {
+    public ZeroAll(Turret turret, ClimbSubsystem climb, LedsSubsystem leds)  {
         addCommands(
             new ZeroTurret(turret),
-            new ZeroClimb(climb)
+            new ClimbZeroing(climb, leds)
         );
     }
 
