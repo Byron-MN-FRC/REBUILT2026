97165c4ed13894f1460262e82de233c11f311967
Paxton Reeve <paxtonreeve@gmail.com>
2026-02-28 10:53:10 -0600

    no more hello

--- src/main/java/frc/robot/commands/FuelGRAB.java
+++ src/main/java/frc/robot/commands/FuelGRAB.java
@@ -51,7 +51,6 @@ public class FuelGRAB extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        System.out.println("HELLO");
         m_subsystem.setFuelGrabberSpeed();
     }
 
