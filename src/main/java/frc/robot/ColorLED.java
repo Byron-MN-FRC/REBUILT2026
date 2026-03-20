7dfa28250efed74b7362b7361fb8b70a473f383e
Paxton Reeve <paxtonreeve@gmail.com>
2026-03-20 14:59:18 -0500

    killing of a bunch of stale files and minor fixes to flashing code

--- src/main/java/frc/robot/ColorLED.java
+++ src/main/java/frc/robot/ColorLED.java
@@ -105,16 +105,13 @@ public class ColorLED {
             for (var i = 0; i < m_ledBuffer.getLength(); i++) {
                 m_ledBuffer.setRGB(i, 0, 0, 0);
             }
-            while (m_timer.get() > 0.5) {
+            if (m_timer.get() > 0.5) {
                 m_timer.reset();
             }
         }
         else if (m_timer.get() <= 0.25) {
             for (var i = 0; i < m_ledBuffer.getLength(); i++) {
                 m_ledBuffer.setRGB(i, 0, 0, 255);
-            }
-            while (m_timer.get() > 1) {
-                m_timer.reset();
             }
         }
         m_led.setData(m_ledBuffer);
@@ -126,16 +123,13 @@ public class ColorLED {
             for (var i = 0; i < m_ledBuffer.getLength(); i++) {
                 m_ledBuffer.setRGB(i, 0, 0, 0);
             }
-            while (m_timer.get() > 0.5) {
+            if (m_timer.get() > 0.5) {
                 m_timer.reset();
             }
         }
         else if (m_timer.get() <= 0.25) {
             for (var i = 0; i < m_ledBuffer.getLength(); i++) {
                 m_ledBuffer.setRGB(i, 17, 125, 0);
-            }
-            while (m_timer.get() > 1) {
-                m_timer.reset();
             }
         }
         m_led.setData(m_ledBuffer);
